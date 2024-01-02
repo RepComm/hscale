4dba156df259609f631a3fca2a955519f3061907
Jon Crowder <rccommandotac@gmail.com>
2024-01-02 11:44:22 -0600

    ./start.sh checks ports before launching now, added -d detach mode switch, check if container name exists

--- hscale/start.sh
+++ hscale/start.sh
@@ -1,45 +1,95 @@
 #!/bin/bash
 
+# the name of the image we're using
 IMAGE_NAME=$(basename $(pwd))
 
+# the partial name of the container we're instancing
+# we prefix hscale- to the beginning later
 HSCALE_CNAME=$1
 if [[ -z $HSCALE_CNAME ]]; then
   HSCALE_CNAME=$IMAGE_NAME
 fi
 
+# the full container name
+FULL_CNAME=hscale-$HSCALE_CNAME
+
+# pocketbase port ENV variable
 POCKET_PORT=$2
 if [[ -z $POCKET_PORT ]]; then
   POCKET_PORT=8090
 fi
 
+# marmot port ENV variable
 MARMOT_PORT=$3
 if [[ -z $MARMOT_PORT ]]; then
   MARMOT_PORT=8091
 fi
 
+# hscale deno http port ENV variable
 HSCALE_PORT=$4
 if [[ -z $HSCALE_PORT ]]; then
   HSCALE_PORT=8092
 fi
 
-echo HSCALE_CNAME: $HSCALE_CNAME , HSCALE_PORT: $HSCALE_PORT , MARMOT_PORT: $MARMOT_PORT , POCKET_PORT: $POCKET_PORT
+DETACH=$5
+if [[ $DETACH = "-d" ]]; then
+  echo "[INFO] Will run in detached mode - webconsole will be available at http://localhost:$HSCALE_PORT/ui"
+else
+  DETACH=""
+fi
+
+PORTS_USED=false
+
+echo "[CHECK] ports '$POCKET_PORT' '$MARMOT_PORT' '$HSCALE_PORT' in use"
+if nc -z localhost $POCKET_PORT; then
+  echo "[ISSUE] Port $POCKET_PORT already in use"
+  PORTS_USED=true
+fi
+
+if nc -z localhost $MARMOT_PORT; then
+  echo "[ISSUE] Port $MARMOT_PORT already in use"
+  PORTS_USED=true
+fi
+
+if nc -z localhost $HSCALE_PORT; then
+  echo "[ISSUE] Port $HSCALE_PORT already in use"
+  PORTS_USED=true
+fi
+
+if [ $PORTS_USED = true ]; then
+  echo "[INFO] Cannot start with ports that are already in use, exiting"
+  exit 2
+fi
+
+echo "[INFO] Specified ports were available"
+echo "[CHECK] hscale-$HSCALE_CNAME used by another container"
+
+if podman container exists $FULL_CNAME; then
+  echo "[ISSUE] Container partial name '$HSCALE_CNAME' aka full name '$FULL_CNAME' already exists. exiting."
+  exit 2
+fi
+
+# echo FULL_CNAME: $FULL_CNAME , HSCALE_PORT: $HSCALE_PORT , MARMOT_PORT: $MARMOT_PORT , POCKET_PORT: $POCKET_PORT
 
 PERSIST_VOL=${HSCALE_CNAME}_persist
-echo Now checking if persistence volume exists for "$PERSIST_VOL"
+echo "[CHECK] volume '$PERSIST_VOL' exists"
 if podman volume exists $PERSIST_VOL; then
-  echo Volume exists, moving on
+  echo "[INFO] volume exists"
 else
-  echo "Creating volume '$PERSIST_VOL' as it didn't exist"
+  echo "[ACTION] Creating volume '$PERSIST_VOL' as it didn't exist"
 
   if ! podman volume create $PERSIST_VOL; then
-    echo "Failed to create volume"
+    echo "[ISSUE] Failed to create volume"
     exit 2
   fi
 fi
 
+echo "[INFO] Starting container"
+
 podman run \
---name hscale-$HSCALE_CNAME \
+--name $FULL_CNAME \
 -v $PERSIST_VOL:/persist \
+$DETACH \
 -e HSCALE_PORT=$HSCALE_PORT \
 -e POCKET_PORT=$POCKET_PORT \
 -e MARMOT_PORT=$MARMOT_PORT \
@@ -51,4 +101,3 @@ deno run --allow-net --allow-read --allow-run --allow-env /hscale/main.ts
 
 
 # --net podman1 \
-# --detach \
--- hscale/webconsole/src/pages/Home.tsx
+++ hscale/webconsole/src/pages/Home.tsx
@@ -12,14 +12,14 @@ interface State {
 }
 
 interface Pod {
-	label: string;
-	id: string;
-	hostname: string;
-	status: string;
+  label: string;
+  id: string;
+  hostname: string;
+  status: string;
 }
 
 export class Home extends Component<Props, State> {
-	async componentDidMount() {
+  async componentDidMount() {
     const raw = await fetch("/api/status/all");
     const res = await raw.json();
 
@@ -29,87 +29,127 @@ export class Home extends Component<Props, State> {
     });
   }
   renderPod(pod: Pod) {
-		return <div class="pod border">
-			<div class="kv">
-				<span class="k">Label : </span><span class="v">{pod.label}</span>
-			</div>
+    return <div class="pod border">
+      <div class="kv">
+        <span class="k">Label : </span><span class="v">{pod.label}</span>
+      </div>
 
-			<div class="kv">
-				<span class="k">ID : </span> <span class="v">{pod.id}</span>
-			</div>
+      <div class="kv">
+        <span class="k">ID : </span> <span class="v">{pod.id}</span>
+      </div>
 
-			<div class="kv">
-				<span class="k">Hostname : </span><span class="v">{pod.hostname}</span>
-			</div>
+      <div class="kv">
+        <span class="k">Hostname : </span><span class="v">{pod.hostname}</span>
+      </div>
 
-			<div class="kv">
-				<span class="k">Status : </span><span class="v">{pod.status}</span>
-			</div>
-		</div>
-	}
-	renderPods(pods: Pod[]) {
-		const results = [];
+      <div class="kv">
+        <span class="k">Status : </span><span class="v">{pod.status}</span>
+      </div>
+    </div>
+  }
+  renderPods(pods: Pod[]) {
+    const results = [];
 
-		for (const pod of pods) {
-			results.push(this.renderPod(pod));
-		}
+    for (const pod of pods) {
+      results.push(this.renderPod(pod));
+    }
 
-		return results;
-	}
-	render() {
-		const pods = [
-			{
-				label: "Demo Pod",
-				hostname: "localhost",
-				id: "abcdefghijklmnopqrstuvwxyz",
-				status: "online"
-			}
-		];
+    return results;
+  }
+  render() {
+    const pods = [
+      {
+        label: "Demo Pod",
+        hostname: "localhost",
+        id: "abcdefghijklmnopqrstuvwxyz",
+        status: "online"
+      }
+    ];
 
     const hostRef = useRef<HTMLInputElement>();
 
-		return <div class="home border">
-			<h1 class="text-center">hscale - webconsole</h1>
-			<div class="section border">
-				<h2 class="text-center">Local</h2>
-				<div class="row">
-					<h3 title="Target mode for replication">Mode</h3>
-					<div class="col">
-						<select class="dropdown align-flex-end">
-							<option title="Data can be synced to new nodes">Seed</option>
-							<option title="Data can be replicated to linked nodes">Replicate</option>
-							<option title="Data incomplete, tries to sync with cluster">Bootstrap</option>
-						</select>
-					</div>
-				</div>
+    return <div class="home border">
+      <h1 class="text-center header">hscale - webconsole</h1>
+      <div class="section border">
+        <h2 class="text-center header">Local</h2>
+        <div class="row">
+          <h2 title="a database in one executable">hscale</h2>
+        </div>
+        <div class="row indent">
+          <h3 for="b-replicate">Replicate</h3>
+          <input id="b-replicate" type="checkbox"></input>
 
-				<div class="row">
-					<h3 title="Replication status">Status :</h3>
-					<div class="col">
-						<h3 class="align-flex-end"
-							title="syncing database from cluster for the first time"
-						>Bootstrapping</h3>
-					</div>
-				</div>
+          <h3 class="indent" title="Replication status">Status :</h3>
+          <h3
+            class="align-flex-end"
+            title="syncing database from cluster for the first time"
+          >Replicating</h3>
+        </div>
+        <div class="row indent">
+          <h3 for="b-replicate">Seed</h3>
+          <input id="b-replicate" type="checkbox"></input>
 
-				<div class="pocketbase border">
-					<div class="row">
-						<h2 title="a database in one executable">pocketbase</h2>
-						<button
+          <h3 class="indent" title="Replication status">Status :</h3>
+          <h3
+            class="align-flex-end"
+            title="syncing database from cluster for the first time"
+          >Seeding</h3>
+        </div>
+        <div class="row indent">
+          <h3 for="b-bootstrap">Rebuild Data</h3>
+          <div class="col">
+            <button
+              class="btn"
+              id="bootstrap"
+              onClick={()=>{
+                const KEY = "HSCALE";
+
+                if (
+                  confirm(
+                    "DANGER: Bootstrap will rebuild local db from a Seed enabled node in the existing cluster. If a seed is not found then no data will change. Please confirm:"
+                  ) && prompt(
+                    `Please enter: ${KEY} to confirm data loss is allowed:`, ""
+                  ) === KEY
+                ) {
+                  fetch("/api/bootstrap");
+                } else {
+                  alert("Bootstrap action is cancelled due to user input.");
+                }
+              }}
+              >Bootstrap</button>
+          </div>
+          <h3 class="indent" title="Replication status">Status :</h3>
+          <h3
+            class="align-flex-end"
+            title="syncing database from cluster for the first time"
+          >Bootstrapping</h3>
+        </div>
+
+        <div class="row">
+          <h3 class="indent" title="Unlinked cluster nodes will be pinged if active">Discoverable</h3>
+          <div class="col">
+            <div class="btn align-flex-end">ACTIVE</div>
+          </div>
+        </div>
+
+        <div class="pocketbase border">
+          <div class="row">
+            <h2 title="a database in one executable">pocketbase</h2>
+            <button
               class="btn"
               title="opens pocketbase web console"
-              onClick={()=>{
+              onClick={() => {
                 const proto = window.location.protocol;
                 const hostname = window.location.hostname;
                 window.open("/pb");
               }}
               disabled={!this.state.pbOnline}
-              >manage</button>
-						<div class="col">
-							<div
-								class="btn power align-flex-end"
-								title="Toggle container online"
-                onClick={async()=>{
+            >manage</button>
+            <div class="col">
+              <div
+                class="btn power align-flex-end"
+                title="Toggle container online"
+                onClick={async () => {
                   const raw = await fetch("/api/toggle/pb");
                   const res = await raw.json();
 
@@ -121,21 +161,21 @@ export class Home extends Component<Props, State> {
                     console.warn("Failed to toggle pb");
                   }
                 }}
-							>
-								<span>{ this.state.pbOnline ? "Power Off" : "Power On"}</span>
-								<div class={this.state.pbOnline ? "on" : "off"} />
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="marmot border">
-					<div class="row">
-						<h2 title="an sqlite replicator">marmot</h2>
-						<div class="col">
-							<div
-								class="btn power align-flex-end"
-								title="Toggle container online"
-                onClick={async()=>{
+              >
+                <span>{this.state.pbOnline ? "Power Off" : "Power On"}</span>
+                <div class={this.state.pbOnline ? "on" : "off"} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="marmot border">
+          <div class="row">
+            <h2 title="an sqlite replicator">marmot</h2>
+            <div class="col">
+              <div
+                class="btn power align-flex-end"
+                title="Toggle container online"
+                onClick={async () => {
                   const raw = await fetch("/api/toggle/marmot");
                   const res = await raw.json();
 
@@ -147,67 +187,61 @@ export class Home extends Component<Props, State> {
                     console.warn("Failed to toggle marmot");
                   }
                 }}
-							>
-								<span>{ this.state.marmotOnline ? "Power Off" : "Power On"}</span>
-								<div class={this.state.marmotOnline ? "on" : "off"} />
-							</div>
-						</div>
-					</div>
+              >
+                <span>{this.state.marmotOnline ? "Power Off" : "Power On"}</span>
+                <div class={this.state.marmotOnline ? "on" : "off"} />
+              </div>
+            </div>
+          </div>
 
-					<div class="row">
-						<h3 title="Human friendly name">Node Label</h3>
-						<div class="col">
-							<input class="align-flex-end" />
-						</div>
-					</div>
-					<div class="row">
-						<h3 title="Marmot port, should be accessible from cluster">Node Port</h3>
-						<div class="col">
-							<input class="align-flex-end" value="4221" />
-						</div>
-					</div>
-					<div class="row">
-						<h3 title="Unlinked cluster nodes will be pinged if active">Discoverable</h3>
-						<div class="col">
-							<div class="btn align-flex-end">ACTIVE</div>
-						</div>
-					</div>
-					<div class="row">
-						<h3 title="container status">Status</h3>
-						<div class="col">
-							<div class="align-flex-end">Online</div>
-						</div>
-					</div>
+          <div class="row">
+            <h3 class="indent" title="Human friendly name">Node Label</h3>
+            <div class="col">
+              <input class="align-flex-end" />
+            </div>
+          </div>
+          <div class="row">
+            <h3 class="indent" title="Marmot port, should be accessible from cluster">Node Port</h3>
+            <div class="col">
+              <input class="align-flex-end" value="4221" />
+            </div>
+          </div>
+          <div class="row">
+            <h3 class="indent" title="container status">Status</h3>
+            <div class="col">
+              <div class="align-flex-end">Online</div>
+            </div>
+          </div>
 
-				</div>
-			</div>
+        </div>
+      </div>
 
-			<div class="section border">
-				<h2 class="text-center">Cluster</h2>
-				<div class="pod border">
-					<div class="row">
-						<div class="add"
-              onClick={async()=>{
+      <div class="section border">
+        <h2 class="text-center header">Cluster</h2>
+        <div class="pod border">
+          <div class="row">
+            <div class="add"
+              onClick={async () => {
                 const raw = await fetch(`/api/add/${hostRef.current.value}`);
                 const res = await raw.json();
 
                 if (res.status === "success") {
-                  
+
                 }
               }}
             />
-						<label for="host-input">Host : </label>
-						<div class="col">
-							<input
+            <label for="host-input">Host : </label>
+            <div class="col">
+              <input
                 ref={hostRef}
-								class="align-flex-end"
-								id="host-input" value="localhost:4221" />
-						</div>
-					</div>
-				</div>
-				{this.renderPods(pods)}
-			</div>
-		</div>
-	}
+                class="align-flex-end"
+                id="host-input" value="localhost:4221" />
+            </div>
+          </div>
+        </div>
+        {this.renderPods(pods)}
+      </div>
+    </div>
+  }
 }
 
